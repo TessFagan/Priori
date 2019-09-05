3ac322aa4746949453bb1714460a4ac209473e9b
alvaro <alvaro@alvaros-mbp.lan>
2019-09-05 04:29:28 -0400

    signup true

--- client/src/App.js
+++ client/src/App.js
@@ -67,7 +67,8 @@ class App extends Component {
               <Route
                 exact path="/signup"
                 render={() =>
-                  <Signup updateUser={this.updateUser} loggedIn={this.state.loggedIn} />} />
+                  <Signup updateUser={this.updateUser} loggedIn={true} />} />
+              {/* {this.state.loggedIn} />} /> */}
               <Route
                 exact path="/macrohome"
                 render={() =>
