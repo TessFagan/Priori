2eb60c535bbb60b37e00fa6886ab950a70345793
alvaro <alvaro@alvaros-mbp.lan>
2019-09-04 22:38:03 -0400

    IV

--- server.js
+++ server.js
@@ -88,10 +88,10 @@ app.use(routes)
 
 // app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
